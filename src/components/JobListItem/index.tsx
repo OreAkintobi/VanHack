e6d22994df6f00ad10ed649307de2878f534aaf5
Ore Akintobi <oreakintobi@gmail.com>
2021-10-18 13:52:37 +0100

    updatd design to JobListItem

--- src/components/JobListItem/index.tsx
+++ src/components/JobListItem/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
 import {JobsData} from '../../types';
+import {VanHackLogo} from '../../assets';
+import {countries} from '../../data';
 
 import styles from './styles';
+import BottomTab from '../BottomTab';
 
 interface JobListItemProps {
   job: JobsData;
@@ -10,21 +13,56 @@ interface JobListItemProps {
 }
 
 const JobListItem = ({job, onPressJobItem}: JobListItemProps) => {
+  const relocationStatus =
+    job?.relocate === 'RemoteRelocate'
+      ? 'Remote, then Relocate'
+      : job?.relocate;
+
+  const country = countries.filter(item => item.code === job?.flagCode)[0]
+    ?.name;
+
   return (
-    <TouchableOpacity onPress={onPressJobItem}>
-      <View style={styles.jobItemContainer} key={job.id}>
-        <View style={styles.dataContainer}>
-          <Text>Title: </Text>
-          <Text>{job.title}</Text>
+    <TouchableOpacity
+      style={styles.touchableContainer}
+      onPress={onPressJobItem}>
+      <View style={styles.jobItemContainer} key={job?.id}>
+        <View style={styles.jobImage}>
+          <Image
+            source={VanHackLogo}
+            resizeMode="contain"
+            style={styles.imageStyle}
+          />
         </View>
-        <View style={styles.dataContainer}>
-          <Text>Location: </Text>
-          <Text>{job.location}</Text>
+
+        <View style={styles.jobContentView}>
+          <View style={styles.dataContainer}>
+            <Text numberOfLines={1} style={styles.jobTitleText}>
+              {job?.title}
+            </Text>
+          </View>
+          <View style={styles.dataContainer}>
+            <Text numberOfLines={1} style={styles.jobText}>
+              {job?.location}
+            </Text>
+          </View>
+          <View style={styles.dataContainer}>
+            <Text numberOfLines={1} style={styles.jobText}>
+              {job?.companyName}
+            </Text>
+          </View>
+          <View style={styles.dataContainer}>
+            <Text numberOfLines={1} style={styles.jobText}>
+              {job?.currency} {job?.salaryFrom} - {job?.salaryTo}
+            </Text>
+          </View>
         </View>
-        <View style={styles.dataContainer}>
-          <Text>Company: </Text>
-          <Text>{job.companyName}</Text>
-        </View>
+      </View>
+
+      <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+        <BottomTab title="Full Time" />
+        <BottomTab title={country} />
+        <BottomTab title={relocationStatus} />
+        <BottomTab title={job?.jobType} />
       </View>
     </TouchableOpacity>
   );
--- src/components/JobListItem/styles.ts
+++ src/components/JobListItem/styles.ts
@@ -1,11 +1,46 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
+  touchableContainer: {
+    marginBottom: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: '#1c1c25',
+    borderRadius: 12,
+    justifyContent: 'space-between',
+    // height: 110,
+  },
   jobItemContainer: {
-    marginVertical: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flex: 1,
+  },
+  jobContentView: {
+    flex: 5,
   },
   dataContainer: {
     flexDirection: 'row',
+  },
+  jobImage: {
+    marginRight: 16,
+  },
+  imageStyle: {
+    height: 60,
+    aspectRatio: 1,
+    borderRadius: 12,
+  },
+  jobTitleText: {
+    fontSize: 14,
+    color: 'white',
+    lineHeight: 16,
+    fontWeight: 'bold',
+    marginBottom: 4,
+  },
+  jobText: {
+    fontSize: 10,
+    color: '#3a3b43',
+    lineHeight: 12,
+    marginBottom: 2,
   },
 });
 
