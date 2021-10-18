69027f56047ab131d8706b952562813a004ea71e
Ore Akintobi <oreakintobi@gmail.com>
2021-10-18 18:26:06 +0100

    Updated design of JobScreen

--- src/screens/JobScreen/index.tsx
+++ src/screens/JobScreen/index.tsx
@@ -1,66 +1,99 @@
-import React, {createRef} from 'react';
-import {Button, View, Text} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {View, Text, Image, ScrollView, TouchableOpacity} from 'react-native';
 import styles from './styles';
 import {JobsData} from '../../types';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../navigation';
-import {RNCamera} from 'react-native-camera';
+import {VanHackLogo} from '../../assets';
+import {colors} from '../../theme';
+import {getCountry, getRelocationStatus, getSalary} from '../../utils';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Job'>;
 
 const JobScreen = ({route, navigation}: Props) => {
   const {params} = route;
   const job: JobsData = params?.job;
-  const camera = createRef<any>();
-
-  const submit = async () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const {uri, codec = 'mp4'} = await camera?.current?.recordAsync();
-    console.info(uri);
-    if (uri.trim() !== ' ' || uri !== undefined || uri !== null) {
-      navigation.navigate('Success');
-    }
-  };
-
-  const stop = async () => {
-    await camera?.current?.stopRecording();
-  };
-
-  const renderCam = () => {
-    return (
-      <RNCamera
-        ref={camera}
-        style={styles.preview}
-        defaultVideoQuality={RNCamera.Constants.VideoQuality['480p']}
-        type={RNCamera.Constants.Type.back}
-        flashMode={RNCamera.Constants.FlashMode.on}
-        androidCameraPermissionOptions={{
-          title: 'Permission to use camera',
-          message: 'We need your permission to use your camera',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}
-        androidRecordAudioPermissionOptions={{
-          title: 'Permission to use audio recording',
-          message: 'We need your permission to use your audio',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}
-      />
-    );
-  };
-
   return (
     <View style={styles.container}>
-      <Text>Company: {job.companyName}</Text>
-      <Text>Job Type: {job.jobType}</Text>
-      <Button title="Apply" onPress={submit} />
-      <Button title="Stop" onPress={stop} />
+      <View style={styles.topSection}>
+        <View style={{alignItems: 'center'}}>
+          <View style={styles.jobImage}>
+            <Image
+              source={VanHackLogo}
+              resizeMode="contain"
+              style={styles.imageStyle}
+            />
+          </View>
 
-      <View style={styles.container}>
-        <View style={{flexDirection: 'row'}}>
-          <View style={{width: '70%'}}>{renderCam()}</View>
+          <Text style={styles.jobTitleText}>{job?.title}</Text>
+
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Text style={styles.jobText}>
+              {job.companyName} <Text style={styles.interpunct}> · </Text>{' '}
+              <Text style={styles.jobText2}>{job.location}</Text>
+            </Text>
+          </View>
         </View>
+
+        <View style={styles.topDetailsContainer}>
+          <View style={styles.detailView}>
+            <Text style={styles.jobText2}>Salary</Text>
+            <Text style={styles.jobText}>{getSalary(job)}</Text>
+          </View>
+
+          <Text style={styles.bar}>|</Text>
+
+          <View style={styles.detailView}>
+            <Text style={styles.jobText2}>Country</Text>
+            <Text style={styles.jobText}>{getCountry(job)}</Text>
+          </View>
+        </View>
+
+        <View style={styles.topDetailsContainer}>
+          <View style={styles.detailView}>
+            <Text style={styles.jobText2}>Job Type</Text>
+            <Text style={styles.jobText}>{job?.jobType}</Text>
+          </View>
+
+          <Text style={styles.bar}>|</Text>
+
+          <View style={styles.detailView}>
+            <Text style={styles.jobText2}>Area</Text>
+            <Text style={styles.jobText}>{job?.area}</Text>
+          </View>
+
+          <Text style={styles.bar}>|</Text>
+
+          <View style={styles.detailView}>
+            <Text style={styles.jobText2}>Option</Text>
+            <Text style={styles.jobText}>{getRelocationStatus(job)}</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={{flex: 4, justifyContent: 'flex-start'}}>
+        <ScrollView>
+          <View style={{marginVertical: 16}}>
+            <Text style={[styles.jobText, {color: colors.white}]}>
+              Overview
+            </Text>
+            <Text style={styles.jobText}>{job?.description}</Text>
+          </View>
+
+          <View style={{marginVertical: 16}}>
+            <Text style={[styles.jobText, {color: colors.white}]}>Skills</Text>
+            <Text style={styles.jobText}>{job?.skills}</Text>
+          </View>
+        </ScrollView>
+      </View>
+
+      <View style={{flex: 1.5}}>
+        <TouchableOpacity
+          style={styles.applyButton}
+          onPress={() => navigation.navigate('Camera')}>
+          <Text style={styles.applyButtonText}>Apply Now</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
--- src/screens/JobScreen/styles.ts
+++ src/screens/JobScreen/styles.ts
@@ -4,21 +4,72 @@ import {colors} from '../../theme';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: colors.black,
+    paddingHorizontal: 16,
+    justifyContent: 'space-between',
+  },
+  topSection: {
+    flex: 4,
+  },
+  jobImage: {
+    // marginRight: 16,
+  },
+  jobTitleText: {
+    fontSize: 20,
+    fontWeight: '800',
+    color: colors.white,
+  },
+  jobText: {
+    fontSize: 12,
+    color: colors.vanHackBlue,
+    marginBottom: 2,
+  },
+  jobText2: {
+    fontSize: 12,
+    color: colors.lightGray,
+    marginBottom: 2,
+  },
+  interpunct: {
+    fontSize: 16,
+  },
+  imageStyle: {
+    height: 100,
+    aspectRatio: 1,
+    borderRadius: 12,
+    marginBottom: 24,
+  },
+  topDetailsContainer: {
+    width: '100%',
+    marginVertical: 12,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderRadius: 4,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderColor: colors.darkGray,
+    borderWidth: 1,
+  },
+  detailView: {
+    marginHorizontal: 8,
+    justifyContent: 'center',
+  },
+  bar: {
+    color: colors.darkGray,
+    fontSize: 16,
+  },
+  applyButton: {
+    width: '100%',
+    backgroundColor: colors.vanHackBlue,
+    borderRadius: 8,
+    paddingVertical: 16,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  capture: {
-    flex: 0,
-    backgroundColor: colors.white,
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20,
-  },
-  preview: {
-    width: '100%',
-    height: '55%',
+  applyButtonText: {
+    color: colors.white,
+    fontSize: 16,
+    fontWeight: '700',
   },
 });
 
