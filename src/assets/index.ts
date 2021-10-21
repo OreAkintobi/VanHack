24dea50341a011fa827035d61f27a15b08b49c57
Ore Akintobi <oreakintobi@gmail.com>
2021-10-22 00:46:22 +0100

    clear project

--- src/assets/index.ts
+++
@@ -1,1 +0,0 @@
-export const VanHackLogo = require('./vanHackLogo.jpeg');
--- src/screens/index.ts
+++
@@ -1,6 +0,0 @@
-import CameraScreen from './CameraScreen';
-import JobScreen from './JobScreen';
-import JobsScreen from './JobsScreen';
-import SuccessScreen from './SuccessScreen';
-
-export {CameraScreen, JobScreen, JobsScreen, SuccessScreen};
